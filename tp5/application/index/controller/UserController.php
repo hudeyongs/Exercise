2da5d3807d674ddf56900831fae0fdbe88e33c6c
胡德咏 <sniffrose@outlook.com>
2017-09-14 17:40:24 +0800

    六, 模型关联 -> 关联

--- tp5/application/index/controller/UserController.php
+++ tp5/application/index/controller/UserController.php
@@ -1,61 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * UserController: deyong@hackoops.com
- * Date: 2017/9/13
- * Time: 17:09
- */
-
 namespace app\index\controller;
 
 use app\index\model\Profile;
-use app\index\model\User;
+use app\index\model\User as UserModel;
 
-class UserController {
-
-	public function index()
+class UserController
+{
+// 关联新增数据
+	public function add()
 	{
-		$list = User::scope('email', 'thinkphp@qq.com')->select();
-		foreach($list as $user)
-		{
-			echo $user->nickname . '<br />';
-			echo $user->email . '<br />';
-			echo $user->birthday . '<br />';
-			echo '---------------<br />';
-		}
-	}
-
-	public function add ()
-	{
-		$user = new User;
-		$user->name = 'thinkphp';
+		$user           = new UserModel;
+		$user->name     = 'thinkphp';
 		$user->password = '123456';
 		$user->nickname = '流年';
-		if($user->save())
-		{
-			$profile = new Profile;
+		if ($user->save()) {
+			// 写入关联数据
+			$profile           = new Profile;
 			$profile->truename = '刘晨';
 			$profile->birthday = '1977-03-05';
-			$profile->address = '中国上海';
-			$profile->email = 'thinkphp@qq.com';
+			$profile->address  = '中国上海';
+			$profile->email    = 'thinkphp@qq.com';
 			$user->profile()->save($profile);
-			return '用户[ ' . $user->name . ' ]新增成功';
-		} else {
-			return $user->getError();
-		}
-	}
-
-
-	public function addList()
-	{
-		$user = new User;
-		$list = [
-			['nickname' => '张三', 'email' => 'zhangsan@qq.com', 'birthday' => strtotime('1998-01-15')],
-			['nickname' => '李四', 'email' => 'lisi@qq.com', 'birthday' => strtotime('1990-09-19')],
-		];
-		if ($user->saveAll($list))
-		{
-			return '用户批量新增成功!';
+			return '用户新增成功';
 		} else {
 			return $user->getError();
 		}
@@ -63,69 +29,10 @@ class UserController {
 
 	public function read($id)
 	{
-		$user = User::get($id);
-		echo $user->name . '<br/>';
-		echo $user->nickname . '<br/>';
-		echo $user->profile->truename . '<br/>';
-		echo $user->profile->email . '<br/>';
-	}
-
-	public function create()
-	{
-		return view();
-	}
-
-	public function update($id)
-	{
-		$user['id'] = (int)$id;
-		$user['nickname'] = '刘晨';
-		$user['email'] = 'liu21st@gmail.com';
-		User::update($user);
-		return '更新用户成功';
-	}
-
-	public function delete ($id)
-	{
-		$user = User::destroy($id);
-		if ($user)
-		{
-			return '删除用户成功';
-		} else {
-			return '删除的用户不存在';
-		}
-	}
-
-	public function profile()
-	{
-		return $this->hasOne('app\index\Profile');
+		$user = UserModel::get($id, 'profile');
+		echo $user->name . '<br />';
+		echo $user->nickname . '<br />';
+		echo $user->profile->truename . '<br />';
+		echo $user->profile->email .'<br />';
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tp5/application/index/model/User.php
+++ tp5/application/index/model/User.php
@@ -1,56 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * UserController: deyong@hackoops.com
- * Date: 2017/9/13
- * Time: 16:55
- */
 namespace app\index\model;
 
 use think\Model;
-class User extends Model {
-// 定义类型转换
-	protected $type = [
-		'birthday'    => 'timestamp:Y/m/d',
-	];
+
+class User extends Model
+{
+	// 开启自动写入时间戳
+	protected $autoWriteTimestamp = true;
+
 	// 定义自动完成的属性
-	protected $insert = ['status'];
+	protected $insert = ['status' => 1];
 
-	// status属性修改器
-	protected function setStatusAttr($value, $data)
-	{
-		return '流年' == $data['nickname'] ? 1 : 2;
-	}
-
-	// status属性读取器
-	protected function getStatusAttr($value)
-	{
-		$status = [-1 => '删除', 0 => '禁用', 1 => '正常', 2 => '待审核'];
-		return $status[$value];
-	}
-
-	protected function scopeEmail($query, $email = '')
-	{
-		$query->where('email', $email);
-	}
-
-	protected function scopeStatus($query)
-	{
-		$query->where('status', 1);
-	}
-
-	protected static function base($query)
-	{
-		$query->where('status', 1);
-	}
-
-	public function books()
-	{
-		return $this->hasMany('Book');
-	}
-
+	// 定义关联方法
 	public function profile()
 	{
+		// 用户HAS ONE档案关联
 		return $this->hasOne('Profile');
 	}
 }
